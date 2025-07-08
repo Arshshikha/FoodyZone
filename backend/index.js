197ec210579ea618cfb1d0335a9ab99cfdecc99c
Arshshikha <arshshikhay@gmail.com>
2025-07-08 21:29:10 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const mongoDB =require("./db")
 mongoDB();
 
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:5198");
+    res.setHeader("Access-Control-Allow-Origin","http://localhost:5173");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
